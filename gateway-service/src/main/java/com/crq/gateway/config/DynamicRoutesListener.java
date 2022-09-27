c76df3e90416e36f6ca0f58d8c3a600d7e2ce637
陈蓉琪 <chenrongqi@njbandou.com>
2022-09-27 22:09:54 +0800

    添加文件服务

---
+++ gateway-service/src/main/java/com/crq/gateway/config/DynamicRoutesListener.java
@@ -0,0 +1,42 @@
+package com.crq.gateway.config;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.google.common.util.concurrent.ListenableFuture;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.concurrent.Executor;
+
+/**
+ * @author: crq
+ * @description:
+ * @date: 2022/9/27 19:57
+ */
+@Slf4j
+@Component
+public class DynamicRoutesListener implements Listener {
+    @Resource
+    private GatewayService gatewayService;
+
+    @Override
+    public Executor getExecutor() {
+        log.info("getExecutor");
+        return null;
+    }
+
+    /**
+     * 使用json转换 将plain text变成RouteDefintion
+     * @param configInfo
+     */
+    @Override
+    public void receiveConfigInfo(String configInfo) {
+        log.info("received routes changes {}",configInfo);
+
+        List<RouteDefinition> definitionList = JSON.parseArray(configInfo,RouteDefinition.class);
+        gatewayService.updateRoutes(definitionList);
+    }
+}
---
+++ gateway-service/src/main/java/com/crq/gateway/config/DynamicRoutesLoader.java
@@ -0,0 +1,41 @@
+package com.crq.gateway.config;
+
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.cloud.nacos.NacosConfigProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+/**
+ * @author: crq
+ * @description:
+ * @date: 2022/9/27 20:02
+ */
+
+@Slf4j
+@Configuration
+public class DynamicRoutesLoader implements InitializingBean {
+    @Resource
+    private NacosConfigManager configService;
+
+    @Resource
+    private NacosConfigProperties configProps;
+
+    @Resource
+    private DynamicRoutesListener dynamicRoutesListener;
+
+    private static final String ROUTES_CONFIG = "routes-config.json";
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 首次加载配置
+        String routes = configService.getConfigService().getConfig(ROUTES_CONFIG,configProps.getGroup(),10000);
+        dynamicRoutesListener.receiveConfigInfo(routes);
+
+        // 注册监听器
+        configService.getConfigService().addListener(ROUTES_CONFIG,configProps.getGroup(),dynamicRoutesListener);
+    }
+}
